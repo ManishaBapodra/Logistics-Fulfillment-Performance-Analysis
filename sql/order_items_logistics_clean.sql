1f12cd2e5c20d130f450436b9ae2bb13a42ebefa
Manisha Bapodra <62524702+ManishaBapodra@users.noreply.github.com>
2025-12-01 15:02:33 +0000

    Update order_items_logistics_clean.sql

--- sql/order_items_logistics_clean.sql
+++ sql/order_items_logistics_clean.sql
@@ -14,17 +14,18 @@ SELECT
   oi.returned_at,
 
   --Durations
-  TIMESTAMP_DIFF(oi.shipped_at, oi.created_at, HOUR) AS processing_hours,
-  TIMESTAMP_DIFF(oi.delivered_at, oi.shipped_at, HOUR) AS shipping_hours,
-  TIMESTAMP_DIFF(oi.delivered_at, oi.created_at, HOUR) AS fulfillment_hours,
+  TIMESTAMP_DIFF(oi.shipped_at, oi.created_at, MINUTE) /60.0 AS processing_hours,
+  TIMESTAMP_DIFF(oi.delivered_at, oi.shipped_at, MINUTE) / 60.0 AS shipping_hours,
+  TIMESTAMP_DIFF(oi.delivered_at, oi.created_at, MINUTE) / 60.0 AS fulfillment_hours,
 
   p.distribution_center_id,
+  dc.name as distribution_name,
 
   -- FIX: Normalize “España” → “Spain”
   REPLACE(u.country, 'España', 'Spain') AS country,
 
   u.state,
-  u.city
+  u.city,
 
 --Joins
 FROM `thelook_ecommerce.order_items` oi
@@ -32,12 +33,11 @@ JOIN `thelook_ecommerce.products` p
   ON oi.product_id = p.id
 JOIN `thelook_ecommerce.users` u
   ON oi.user_id = u.id
+JOIN `thelook_ecommerce.distribution_centers` dc
+ON p.distribution_center_id = dc.id
 
 --Data Cleaning
 WHERE
   oi.status NOT IN ('Cancelled', 'Processing')
-  AND oi.created_at IS NOT NULL
-  AND oi.shipped_at IS NOT NULL
-  AND oi.delivered_at IS NOT NULL
   AND oi.shipped_at >= oi.created_at
   AND oi.delivered_at >= oi.shipped_at;
