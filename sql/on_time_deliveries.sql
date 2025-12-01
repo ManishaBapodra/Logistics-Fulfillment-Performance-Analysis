cbd9989ea1f422b9b1084c05ec838686e958015a
Manisha Bapodra <62524702+ManishaBapodra@users.noreply.github.com>
2025-12-01 16:00:48 +0000

    Delete sql/on_time_deliveries.sql

--- sql/on_time_deliveries.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE VIEW `e-commerceanalysis.thelook_ecommerce.logistics_kpi_on_time_delivery` AS
-SELECT
-  COUNTIF(fulfillment_hours <= 240) AS on_time_deliveries,
-  COUNT(*) AS total_deliveries,
-  SAFE_DIVIDE(COUNTIF(fulfillment_hours <= 240), COUNT(*)) AS on_time_delivery_rate
-FROM `e-commerceanalysis.thelook_ecommerce.order_items_logistics_clean`;
