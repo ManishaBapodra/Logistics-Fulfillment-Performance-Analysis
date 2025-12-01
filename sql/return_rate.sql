33544244d7c2ff9960d642c855472bbc3c6b6bb2
Manisha Bapodra <62524702+ManishaBapodra@users.noreply.github.com>
2025-12-01 16:01:01 +0000

    Delete sql/return_rate.sql

--- sql/return_rate.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE VIEW `e-commerceanalysis.thelook_ecommerce.logistics_return_rate` AS
-SELECT
-  COUNTIF(returned_at IS NOT NULL) AS returned_items,
-  COUNT(*) AS total_items,
-  SAFE_DIVIDE(COUNTIF(returned_at IS NOT NULL), COUNT(*)) AS return_rate
-FROM `e-commerceanalysis.thelook_ecommerce.order_items_logistics_clean`;
